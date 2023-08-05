0c3fe71542dd6e5e94f1fc7e8ae8030753b718cb
Ebadulislam1001 <ebadulislamfarooqi@gmail.com>
2023-08-06 00:17:26 +0530

    file rename

---
+++ 009_LinkedList_Intro.cpp
@@ -0,0 +1,46 @@
+// 009 Linked Lists Introduction
+#include <iostream>
+using namespace std;
+
+// Each node in a linked list has two parts:
+// 1. Data part - to store the required data (can have one or more data variables)
+// 2. Link part - to store the link of the next node (and the link of prev node)
+// We achieve this by defining a struct as follows
+struct node
+{
+    char data;  // data part
+    node *next; // link part
+    // struct node *next; (syntax for C)
+};
+
+int main()
+{
+    // Declaring three node to form a linked list
+    node n1, n2, n3, n4, n5;
+
+    // Setting data values
+    n1.data = 'H';
+    n2.data = 'E';
+    n3.data = 'L';
+    n4.data = 'L';
+    n5.data = 'O';
+    // Setting next values
+    n1.next = &n2;  // First node next  part points to Second node
+    n2.next = &n3;  // Second node next part points to Third node
+    n3.next = &n4;  // Third node next part points to Fourth node
+    n4.next = &n5;  // Fourth node next part points to Fifth node
+    n5.next = NULL; // Fifth node next part points to NULL (marks the end of the list)
+
+    node *temp = &n1; // temp pointer pointing to the first node
+    printf("\n\nList Elements ");
+    while (temp != NULL)
+    {
+        // Let temp pointer pointing to the ith node
+        printf("-> %c ", temp->data); // Print the ith character
+        temp = temp->next;            // temp pointer pointing to the (i+1)th node
+    }
+    // (temp == NULL) brings us out of the loop
+    // We have traversed through all the nodes in the Linked List
+    printf("\n\n");
+    return 0;
+}
