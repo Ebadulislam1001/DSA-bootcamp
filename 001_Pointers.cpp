6d459949fcaa8b74e3e2e11a9c0ede35c325c7f4
Ebadul_Islam <98221197+Ebadulislam1001@users.noreply.github.com>
2024-06-02 04:31:46 +0000

    02-06-2024

--- 001_Pointers.cpp
+++ 001_Pointers.cpp
@@ -4,11 +4,22 @@ using namespace std;
 
 int main()
 {
-    // Variable of type int
+    // Like other variables, pointers are also variables
+    // int variables store numbers
+    // char variables store characters
+    // pointer variables store addresses of other variables
+
+    // NOTE: pointer is not a data type though.
+    // data type of a pointer variable depends upon the type of the variable whose address it stores
+    // eg: a pointer pointing to an `char` type variable is of the type `char*`
+    // eg: a pointer pointing to an `int` type variable is of the type `int*`
+    // eg: a pointer pointing to an `int*` type variable is of the type `int**`
+    
+    // A variable of type int
     int a = 24;
-    // Pointing to a variable of type int
+    // A pointer pointing to a variable of type int
     int *p = &a;
-    // Pointer pointing to a pointer pointing to a variable of type int
+    // A pointer pointing to a pointer pointing to a variable of type int
     int **q = &p;
 
     // Read `&` as `address of`. It is used to get the address of a variable (of any type incl pointers).
@@ -18,15 +29,19 @@ int main()
     printf("\t  a = %d\n", a);
     printf("\t *p = %d\n", *p);
     printf("\t**q = %d\n", **q);
+
     printf("\nprinting the address of the variable\n\n");
     printf("\t &a = %d, &a = %x\n", &a, &a);
     printf("\t  p = %d,  p = %x\n", p, p);
     printf("\t *q = %d, *q = %x\n", *q, *q);
+    
     printf("\nprinting the address of the pointer\n\n");
     printf("\t &p = %d, &p = %x\n", &p, &p);
     printf("\t  q = %d,  q = %x\n", q, q);
     printf("\n\n");
+    
     // %d is used to print the address in decimal format (base-10)
     // %x is used to print the address in hexadec format (base-16)
+    // follow up: what is default format if you use `cout` to print the address, decimal or hexadec ?
     return 0;
 }
